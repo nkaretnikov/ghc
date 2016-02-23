63f646a758a76a4524661de9db100f62f7b77789
Nikita Karetnikov <nikita@karetnikov.org>
2016-02-24 02:14:29 +0300

    Remove 'make.sh'

--- make.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-source $HOME/haskell/ghc/envvars.sh
-
-x86_64-elf-gcc $CFLAGS -c -o "${LIBSTUBOBJ}/poll.o" "${LIBSTUBSRC}/poll.c"
-x86_64-elf-ar cr "${LIBSTUBLIB}/libstub.a" "${LIBSTUBOBJ}/poll.o"
-
-make
