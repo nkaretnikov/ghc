54c6864c26e4c3ac9a3cf1f8a8bca2282a7499ef
Nikita Karetnikov <nikita@karetnikov.org>
2016-03-13 03:14:17 +0300

    Build libstub before 'configure'

--- configure.sh
+++ configure.sh
@@ -2,7 +2,12 @@
 
 set -e
 
-source envvars.sh
+source $HOME/haskell/ghc/envvars.sh
+
+x86_64-elf-gcc $CFLAGS -c -o "${LIBSTUBOBJ}/iconv.o" "${LIBSTUBSRC}/iconv.c"
+x86_64-elf-gcc $CFLAGS -c -o "${LIBSTUBOBJ}/poll.o" "${LIBSTUBSRC}/poll.c"
+x86_64-elf-gcc $CFLAGS -c -o "${LIBSTUBOBJ}/times.o" "${LIBSTUBSRC}/times.c"
+x86_64-elf-ar cr "${LIBSTUBLIB}/libstub.a" "${LIBSTUBOBJ}/iconv.o" "${LIBSTUBOBJ}/poll.o" "${LIBSTUBOBJ}/times.o"
 
 ./configure \
   CPP="${BIN_PREFIX}gcc -E" \
--- make.sh
+++ make.sh
@@ -2,11 +2,4 @@
 
 set -e
 
-source $HOME/haskell/ghc/envvars.sh
-
-x86_64-elf-gcc $CFLAGS -c -o "${LIBSTUBOBJ}/iconv.o" "${LIBSTUBSRC}/iconv.c"
-x86_64-elf-gcc $CFLAGS -c -o "${LIBSTUBOBJ}/poll.o" "${LIBSTUBSRC}/poll.c"
-x86_64-elf-gcc $CFLAGS -c -o "${LIBSTUBOBJ}/times.o" "${LIBSTUBSRC}/times.c"
-x86_64-elf-ar cr "${LIBSTUBLIB}/libstub.a" "${LIBSTUBOBJ}/iconv.o" "${LIBSTUBOBJ}/poll.o" "${LIBSTUBOBJ}/times.o"
-
 make
