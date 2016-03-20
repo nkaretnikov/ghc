5cf8614d8d551fdfb430b616787d4a032eac621f
Nikita Karetnikov <nikita@karetnikov.org>
2016-03-21 02:24:28 +0300

    Remove the already provided stubs

--- libstub/src/signal.c
+++ libstub/src/signal.c
@@ -5,24 +5,6 @@
 
 #define SA_RESETHAND 0x80000000
 
-int sigemptyset(sigset_t *set)
-{
-  Print(L"sigemptyset stub called\r\n");
-  return 0;
-}
-
-int sigaddset(sigset_t *set, int signum)
-{
-  Print(L"sigaddset stub called\r\n");
-  return 0;
-}
-
-int sigdelset(sigset_t *set, int signum)
-{
-  Print(L"sigdelset stub called\r\n");
-  return 0;
-}
-
 int sigaction(int signum, const struct sigaction *act,
               struct sigaction *oldact)
 {
