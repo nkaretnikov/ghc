ebdfea77a603ca75deb72fe03acdd648d80efd58
Nikita Karetnikov <nikita@karetnikov.org>
2016-04-18 04:36:30 +0300

    Add <dirent.h> and <sys/resource.h> stubs for unix

--- libshim/include/dirent.h
+++ libshim/include/dirent.h
@@ -1,1 +1,10 @@
-#include_next <dirent.h>
+// Do not include newlib's header since it's unsupported (and triggers an error).
+// #include_next <dirent.h>
+
+#include <sys/types.h>
+
+// XXX: See dirstream.h for a proper definition.
+struct __dirstream
+{
+};
+typedef struct __dirstream DIR;
--- libshim/include/sys/resource.h
+++ libshim/include/sys/resource.h
@@ -1,1 +1,8 @@
 #include_next <sys/resource.h>
+
+#define rlim_t unsigned long int
+struct rlimit
+{
+  rlim_t rlim_cur;
+  rlim_t rlim_max;
+};
