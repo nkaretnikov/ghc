ca0a212f4f18d2639dde7729d8c601768ccb7bc4
Nikita Karetnikov <nikita@karetnikov.org>
2016-04-19 02:04:23 +0300

    Add constants to <sys/resource.h>

--- libshim/include/sys/resource.h
+++ libshim/include/sys/resource.h
@@ -6,3 +6,11 @@ struct rlimit
   rlim_t rlim_cur;
   rlim_t rlim_max;
 };
+
+#define RLIMIT_CPU    0
+#define RLIMIT_FSIZE  1
+#define RLIMIT_DATA   2
+#define RLIMIT_STACK  3
+#define RLIMIT_CORE   4
+#define RLIMIT_NOFILE 7
+#define RLIM_INFINITY 0xffffffffffffffffuLL
