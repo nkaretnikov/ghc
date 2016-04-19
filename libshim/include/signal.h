46eeb3c4c911666725af411cef1433567a4fb7f0
Nikita Karetnikov <nikita@karetnikov.org>
2016-04-19 03:01:20 +0300

    Add a siginfo_t stub

--- libshim/include/signal.h
+++ libshim/include/signal.h
@@ -1,1 +1,12 @@
-#include_next <signal.h>
+// newlib's siginfo_t lacks a few required fields.
+// #include_next <signal.h>
+
+typedef struct
+{
+  int si_signo;
+  int si_code;
+  int si_errno;
+  int si_pid;
+  int si_uid;
+  int si_status;
+} siginfo_t;
