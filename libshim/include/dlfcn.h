c55caf734cacad6a138c3595b009c8cb8a6fa15d
Nikita Karetnikov <nikita@karetnikov.org>
2016-04-20 02:02:29 +0300

    Add constants to dlfcn.h

--- libshim/include/dlfcn.h
+++ libshim/include/dlfcn.h
@@ -4,3 +4,8 @@ void *dlopen(const char *, int);
 void *dlsym(void *, const char *);
 int dlclose(void *);
 char *dlerror(void);
+
+#define RTLD_LAZY    0x00001
+#define RTLD_NOW     0x00002
+#define RTLD_GLOBAL  0x00100
+#define RTLD_LOCAL   0
